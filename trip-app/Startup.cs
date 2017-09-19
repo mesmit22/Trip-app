e9ce635e58bfd8512246b83d80b544d8824aef16
Mark <mark.edwin.smith@gmail.com>
2017-09-19 15:44:35 -0400

    Init Commit

---
+++ trip-app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(trip_app.Startup))]
+namespace trip_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
