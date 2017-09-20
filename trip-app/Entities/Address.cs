72289266ce29037aa7a2e6541e913e5522365a29
Mark <mark.edwin.smith@gmail.com>
2017-09-20 16:11:22 -0400

    Added address entity

---
+++ trip-app/Entities/Address.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace trip_app.Entities
+{
+    [Table("Address")]
+    public class Address
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AddressId { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+
+        [Required]
+        [MaxLength(14)]
+        public string State { get; set; }
+
+        [Required]
+        [MaxLength(26)]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Zip Code")]
+        [MaxLength(5)]
+        public int ZipCode { get; set; }
+    }
+}
