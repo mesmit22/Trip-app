49e4ec445e12c43843b5749d1ce3cdc2f8e80749
Mark <mark.edwin.smith@gmail.com>
2017-09-20 16:59:34 -0400

    Added city entity

---
+++ trip-app/Entities/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace trip_app.Entities
+{
+    public class City
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CityID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Display(Name = "City Name")]
+        public string CityName { get; set; }
+    }
+}
